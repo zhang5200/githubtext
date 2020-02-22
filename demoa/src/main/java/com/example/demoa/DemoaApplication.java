3e4992d10ed983289b42eb74b572251dda7e955e
mac <740436767@qq.com>
2020-02-22 10:00:27 +0800

    修改第一版本

--- demoa/src/main/java/com/example/demoa/DemoaApplication.java
+++ demoa/src/main/java/com/example/demoa/DemoaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoaApplication.class, args);
+        SpringApplication.run(DemoaApplication.class, args
+        System.out.println("这是我的名字谢谢vuo");
+        );
     }
 
 }
