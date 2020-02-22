be8abaebc34c12ddc8fbb5611adb34496d70a23a
mac <740436767@qq.com>
2020-02-22 09:19:22 +0800

    版本002

---
+++ demoa/src/test/java/com/example/demoa/DemoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
