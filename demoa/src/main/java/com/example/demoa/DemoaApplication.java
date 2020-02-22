dc382f164cf610dfb2ddddef9b6b33c1f90e55fb
mac <740436767@qq.com>
2020-02-22 10:10:03 +0800

    修改第三版本

--- demoa/src/main/java/com/example/demoa/DemoaApplication.java
+++ demoa/src/main/java/com/example/demoa/DemoaApplication.java
@@ -9,6 +9,7 @@ public class DemoaApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoaApplication.class, args
         System.out.println("这是我的名字谢谢vuo");
+        System.out.println("李航呀");
         );
     }
 
